31669b9e6306dd7e715ccd74fbbd70524227d6c4
D-Ono <55718959+D-Ono@users.noreply.github.com>
2020-07-16 21:57:41 -0300

    atualizando

--- 02 - Lista/01 - Lista Estatica Sequencial/LEstaticaSequencial.c
+++ 02 - Lista/01 - Lista Estatica Sequencial/LEstaticaSequencial.c
@@ -2,25 +2,37 @@
 #include "LEstaticaSequencial.h"
 
 void InicializaLista(struct lista *L){
+
     for (int i=0; i< MAX; i++){
         L->elem[i] =   0;
     }
     L->qelem =0;
 }
 
-void Inserir(struct lista *L, int elem, int i){
+void Inserir(struct lista *L, int elem, int i){     /// INSERE ELEMENTO NA POSIÇÂO i
     L->elem[i] = elem;
     L->qelem = L->qelem +1;
     return;
 }
 
-void Remover(struct lista *L, int pos){
+void InserirFinal(struct lista *L, int elem){     /// INSERE ELEMENTO NO FINAL DA LISTA
+    L->elem[L->qelem] = elem;
+    L->qelem = L->qelem +1;
+    return;
+}
+
+void Remover(struct lista *L, int pos){             /// REMOVE ELEMENTO NA POSICAO pos
     int i;
     for (i=pos; i<= L->qelem; i++){
         L->elem[i] = L->elem[i+1];
     }
     L->qelem = L->qelem-1;
     return;
+}
+
+void RemoverFinal(struct lista *L){             /// REMOVE ELEMENTO NO FINAL DA LISTA
+    int elem = L->elem[--(L->qelem)];
+    return elem;
 }
 
 int ImprimirRecursivo(struct lista *L, int i){
--- 02 - Lista/01 - Lista Estatica Sequencial/LEstaticaSequencial.h
+++ 02 - Lista/01 - Lista Estatica Sequencial/LEstaticaSequencial.h
@@ -9,3 +9,5 @@ void InicializaLista(struct lista *L);
 void Inserir(struct lista *l, int elem, int i);
 void Remover(struct lista *l, int pos);
 int ImprimirRecursivo(struct lista *l, int i);
+void InserirFinal(struct lista *l, int elem);
+void RemoverFinal(struct lista *l);
